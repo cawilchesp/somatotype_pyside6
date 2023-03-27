bc138c1d45e7898df17f880b975df66b21cc50f4
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-03-27 16:40:51 -0500

    Update patient_ui.py

--- forms/patient_ui.py
+++ forms/patient_ui.py
@@ -1,5 +1,5 @@
 """
-Patient
+Patient Dialog
 
 This file contains class Patient Dialog.
 
@@ -7,40 +7,30 @@ To provide the patient information, it requires:
 
 Last Name
 First Name
-ID Type:
-    CC, TI
+ID Type: CC, TI
 ID Number
 Birth Date
-Sex:
-    F, M
+Sex: F, M
 Weight
 Weight Unit: Kg, Lb
 Height
-Height Unit: m, ft - in
-    Examples for ft - in:
-    Correct:
-    5.09: 5 ft, 9 in
-    5.11: 5 ft, 11 in
-    Wrong:
-    5.9: 5 ft, 90 in
-    5.13: 5 ft, 13 in
-Body Mass Index:
-    BMI is calculated from weight and height values and units
+Height Unit: m, ft'in"
+Body Mass Index: BMI is calculated from weight and height values and units
 """
 
-from PyQt6 import QtWidgets
-from PyQt6.QtCore import QSettings, QRegularExpression
-from PyQt6.QtGui import QRegularExpressionValidator
+from PySide6 import QtWidgets
+from PySide6.QtCore import QSettings
+
+from components.md3_button import MD3Button
+from components.md3_card import MD3Card
+from components.md3_datepicker import MD3DatePicker
+from components.md3_label import MD3Label
+from components.md3_segmentedbutton import MD3SegmentedButton
+from components.md3_textfield import MD3TextField
+from components.md3_window import MD3Window
 
 import sys
 
-from components.md3_window import MD3Window
-from components.md3_card import MD3Card
-from components.md3_textfield import MD3TextField
-from components.md3_label import MD3Label
-from components.md3_segmentedbutton import MD3SegmentedButton
-from components.md3_datepicker import MD3DatePicker
-from components.md3_button import MD3Button
 
 class Patient_UI(QtWidgets.QDialog):
     def __init__(self, parent):
@@ -54,10 +44,6 @@ class Patient_UI(QtWidgets.QDialog):
         self.language_value = int(self.settings.value('language'))
         self.theme_value = eval(self.settings.value('theme'))
 
-        self.regExp1 = QRegularExpressionValidator(QRegularExpression('[A-Za-zÁÉÍÓÚáéíóú ]{1,30}'), self)
-        self.regExp2 = QRegularExpressionValidator(QRegularExpression('[0-9]{1,10}'), self)
-        self.regExp3 = QRegularExpressionValidator(QRegularExpression('[0-9.]{1,5}'), self)
-
         self.patient_widgets = {}
 
         # -------------
@@ -67,10 +53,11 @@ class Patient_UI(QtWidgets.QDialog):
         self.patient_widgets['patient_window'] = MD3Window( {
             'parent': parent, 
             'size': (width, height),
+            'minimum_size': (width, height),
+            'maximum_size': (width, height),
             'labels': ('Datos del Paciente',"Patient's Data"),
             'theme': self.theme_value, 
             'language': self.language_value } )
-        parent.setMaximumSize(width, height)
         
         # -------------
         # Card Paciente
@@ -79,29 +66,30 @@ class Patient_UI(QtWidgets.QDialog):
             'name': 'patient_form_card',
             'position': (8, 8),
             'size': (width-16, height-16),
-            'theme': self.theme_value,
+            'type': 'filled',
             'labels': ('Información del Paciente', 'Patient Information'), 
+            'theme': self.theme_value,
             'language': self.language_value } )
- 
-        self.patient_widgets['last_name_text'] = MD3TextField(self.patient_widgets['patient_form_card'], {
-            'name': 'last_name_text',
+        
+        self.patient_widgets['last_name_textfield'] = MD3TextField(self.patient_widgets['patient_form_card'], {
+            'name': 'last_name_textfield',
             'position': (8, 68),
             'width': width - 32,
             'labels': ('Apellidos', 'Last Name'),
-            'regular_expression': self.regExp1,
+            'type': 'text',
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['last_name_text'].text_field.textEdited.connect(parent.on_last_name_text_textEdited)
-
-        self.patient_widgets['first_name_text'] = MD3TextField(self.patient_widgets['patient_form_card'], {
-            'name': 'first_name_text',
+            'language': self.language_value,
+            'text_edited': parent.on_last_name_textEdited } )
+ 
+        self.patient_widgets['first_name_textfield'] = MD3TextField(self.patient_widgets['patient_form_card'], {
+            'name': 'first_name_textfield',
             'position': (8, 136),
             'width': width - 32,
             'labels': ('Nombres', 'First Name'),
-            'regular_expression': self.regExp1,
+            'type': 'text',
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['first_name_text'].text_field.textEdited.connect(parent.on_first_name_text_textEdited)
+            'language': self.language_value,
+            'text_edited': parent.on_first_name_textEdited } )
 
         self.patient_widgets['id_label'] = MD3Label(self.patient_widgets['patient_form_card'], {
             'name': 'id_label', 
@@ -117,32 +105,35 @@ class Patient_UI(QtWidgets.QDialog):
             'position': (8, 224),
             'width': 76,
             'labels': ('CC', 'CC'),
+            'check_icon': True,
             'location': 'left',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } ) 
-        self.patient_widgets['cc_button'].clicked.connect(parent.on_cc_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_cc_button_clicked } )
 
         self.patient_widgets['ti_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
             'name': 'ti_button',
             'position': (84, 224),
             'width': 76,
             'labels': ('TI', 'TI'),
+            'check_icon': True,
             'location': 'right',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } ) 
-        self.patient_widgets['ti_button'].clicked.connect(parent.on_ti_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_ti_button_clicked } )
 
-        self.patient_widgets['id_text'] = MD3TextField(self.patient_widgets['patient_form_card'], {
-            'name': 'id_text',
+        self.patient_widgets['id_textfield'] = MD3TextField(self.patient_widgets['patient_form_card'], {
+            'name': 'id_textfield',
             'position': (168, 204),
             'width': 156,
             'labels': ('Número de ID', 'ID Number'),
-            'regular_expression': self.regExp2,
+            'type': 'integer',
+            'size': 10,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['id_text'].text_field.textEdited.connect(parent.on_id_text_textEdited)
+            'language': self.language_value,
+            'text_edited': parent.on_id_textEdited } )
 
         self.patient_widgets['birth_date'] = MD3DatePicker(self.patient_widgets['patient_form_card'], {
             'name': 'birth_date',
@@ -161,37 +152,39 @@ class Patient_UI(QtWidgets.QDialog):
             'theme': self.theme_value,
             'language': self.language_value } )
 
-        self.patient_widgets['f_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
-            'name': 'f_button',
+        self.patient_widgets['female_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
+            'name': 'female_button',
             'position': (188, 292),
             'width': 68,
             'labels': ('F', 'F'),
+            'check_icon': True,
             'location': 'left',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['f_button'].clicked.connect(parent.on_f_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_female_button_clicked } )
 
-        self.patient_widgets['m_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
-            'name': 'm_button',
+        self.patient_widgets['male_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
+            'name': 'male_button',
             'position': (256, 292),
             'width': 68,
             'labels': ('M', 'M'),
+            'check_icon': True,
             'location': 'right',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['m_button'].clicked.connect(parent.on_m_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_male_button_clicked } )
 
-        self.patient_widgets['weight_text'] = MD3TextField(self.patient_widgets['patient_form_card'], {
-            'name': 'weight_text',
+        self.patient_widgets['weight_textfield'] = MD3TextField(self.patient_widgets['patient_form_card'], {
+            'name': 'weight_textfield',
             'position': (8, 340),
             'width': 100,
             'labels': ('Peso', 'Weight'),
-            'regular_expression': self.regExp3,
+            'type': 'weight',
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['weight_text'].text_field.textEdited.connect(parent.on_weight_text_textEdited)
+            'language': self.language_value,
+            'text_edited': parent.on_weight_textEdited } )
 
         self.patient_widgets['weight_unit_label'] = MD3Label(self.patient_widgets['patient_form_card'], {
             'name': 'weight_unit_label',
@@ -207,32 +200,34 @@ class Patient_UI(QtWidgets.QDialog):
             'position': (116, 360),
             'width': 76,
             'labels': ('Kg', 'Kg'),
+            'check_icon': True,
             'location': 'left',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['kg_button'].clicked.connect(parent.on_kg_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_kg_button_clicked } )
 
         self.patient_widgets['lb_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
             'name': 'lb_button',
             'position': (192, 360),
             'width': 76,
             'labels': ('Lb', 'Lb'),
+            'check_icon': True,
             'location': 'right',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['lb_button'].clicked.connect(parent.on_lb_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_lb_button_clicked } )
 
-        self.patient_widgets['height_text'] = MD3TextField(self.patient_widgets['patient_form_card'], {
-            'name': 'height_text',
+        self.patient_widgets['height_textfield'] = MD3TextField(self.patient_widgets['patient_form_card'], {
+            'name': 'height_textfield',
             'position': (8, 408),
             'width': 100,
             'labels': ('Altura', 'Height'),
-            'regular_expression': self.regExp3,
+            'type': 'height_si',
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['height_text'].text_field.textEdited.connect(parent.on_height_text_textEdited)
+            'language': self.language_value,
+            'text_edited': parent.on_height_textEdited } )
 
         self.patient_widgets['height_unit_label'] = MD3Label(self.patient_widgets['patient_form_card'], {
             'name': 'height_unit_label',
@@ -243,27 +238,29 @@ class Patient_UI(QtWidgets.QDialog):
             'theme': self.theme_value,
             'language': self.language_value } )
 
-        self.patient_widgets['mt_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
-            'name': 'mt_button',
+        self.patient_widgets['m_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
+            'name': 'm_button',
             'position': (116, 428),
             'width': 76,
             'labels': ('m', 'm'),
+            'check_icon': True,
             'location': 'left',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['mt_button'].clicked.connect(parent.on_mt_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_m_button_clicked } )
 
-        self.patient_widgets['fi_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
-            'name': 'fi_button',
+        self.patient_widgets['ft_in_button'] = MD3SegmentedButton(self.patient_widgets['patient_form_card'], {
+            'name': 'ft_in_button',
             'position': (192, 428),
             'width': 76,
-            'labels': ('ft.in', 'ft.in'),
+            'labels': ('ft\'in\"', 'ft\'in\"'),
+            'check_icon': True,
             'location': 'right',
             'state': False,
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['fi_button'].clicked.connect(parent.on_fi_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_ft_in_button_clicked } )
 
         self.patient_widgets['bmi_label'] = MD3Label(self.patient_widgets['patient_form_card'], {
             'name': 'bmi_label',
@@ -280,10 +277,9 @@ class Patient_UI(QtWidgets.QDialog):
             'width': 164,
             'align': 'center',
             'type': 'value',
-            'color': '255, 255, 255' if self.theme_value else '0, 0, 0',
-            'theme': self.theme_value,
-            'language': self.language_value } )
-
+            'border_color': '#000000' if self.theme_value else '#FFFFFF',
+            'theme': self.theme_value } )
+        
         # ---------------------
         # Buttons Ok and Cancel
         # ---------------------
@@ -294,18 +290,18 @@ class Patient_UI(QtWidgets.QDialog):
             'type': 'text',
             'labels': ('Cancelar', 'Cancel'),
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['cancel_button'].clicked.connect(parent.on_cancel_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_cancel_button_clicked } )
 
         self.patient_widgets['ok_button'] = MD3Button(self.patient_widgets['patient_form_card'], {
             'name': 'ok_button',
             'position': (width - 124, height - 56),
             'width': 100,
             'type': 'text',
+            'enabled': False,
             'labels': ('Aceptar', 'Ok'),
             'theme': self.theme_value,
-            'language': self.language_value } )
-        self.patient_widgets['ok_button'].setEnabled(False)
-        self.patient_widgets['ok_button'].clicked.connect(parent.on_ok_button_clicked)
+            'language': self.language_value,
+            'clicked': parent.on_ok_button_clicked } )
 
         
