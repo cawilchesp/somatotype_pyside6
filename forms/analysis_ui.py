c96e5b7f953d2d57f19e5a89f3272603e0cf4a1f
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-03-29 11:11:29 -0500

    Update analysis_ui.py

--- forms/analysis_ui.py
+++ forms/analysis_ui.py
@@ -1,19 +1,24 @@
 """
-Image
+Analysis
 
-This file contains class Image Dialog.
+This file contains class Analysis Dialog.
 
-To provide the patient information, it requires:
+To provide the analysis information, it requires:
 
-Image Filename
+triceps
+subescapular
+supraespinal
+pantorrilla
 
-Minimum and Maximum value of Left Foot XT Signal
-Minimum and Maximum value of Center Foot XT Signal
-Minimum and Maximum value of Right Foot XT Signal
+altura
+humero
+femur
+biceps
+tricipital
+pantorrilla_perimetro
+pantorrilla_pliegue
 
-Minimum and Maximum value of Left Foot YT Signal
-Minimum and Maximum value of Center Foot YT Signal
-Minimum and Maximum value of Right Foot YT Signal
+weight
 """
 
 from PySide6 import QtWidgets
@@ -21,9 +26,6 @@ from PySide6.QtCore import QSettings
 
 from components.md3_button import MD3Button
 from components.md3_card import MD3Card
-from components.md3_divider import MD3Divider
-from components.md3_iconbutton import MD3IconButton
-from components.md3_label import MD3Label
 from components.md3_textfield import MD3TextField
 from components.md3_window import MD3Window
 
@@ -41,246 +43,201 @@ class AnalysisUI(QtWidgets.QDialog):
         self.language_value = int(self.settings.value('language'))
         self.theme_value = eval(self.settings.value('theme'))
 
-        self.image_widgets = {}
-        minimum_label = ('Mínimo', 'Minimum')
-        maximum_label = ('Máximo', 'Maximum')
+        self.analysis_widgets = {}
 
         # -------------
         # Dialog Window
         # -------------
-        (width, height) = (348, 576)
-        self.image_widgets['image_window'] = MD3Window( {
+        (width, height) = (500, 540)
+        self.analysis_widgets['image_window'] = MD3Window( {
             'parent': parent, 
             'size': (width, height),
             'minimum_size': (width, height),
             'maximum_size': (width, height),
-            'labels': ('Datos de la Imagen', 'Image Data'),
+            'labels': ('Datos del Somatotipo', 'Somatotype Data'),
             'theme': self.theme_value, 
             'language': self.language_value } )
 
         # ---------------
-        # Card Image Form
+        # Card Endomorphy
         # ---------------
-        self.image_widgets['image_form_card'] = MD3Card(parent, { 
-            'name': 'image_form_card',
+        self.analysis_widgets['endomorph_card'] = MD3Card(parent, { 
+            'name': 'endomorph_card',
             'position': (8, 8),
-            'size': (width-16, height - 16), 
+            'size': ((width / 2) - 12, 288), 
             'type': 'filled',
-            'labels': ('Información de la Imagen', 'Image Information'),
+            'labels': ('Endomorfismo', 'Endomorphy'),
             'theme': self.theme_value,
             'language': self.language_value } )
 
-        self.image_widgets['filename_textfield'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'filename_textfield',
-            'position': (8, 68),
-            'width': width - 72,
-            'labels': ('Archivo de Imagen', 'Image File'),
+        self.analysis_widgets['triceps_text'] = MD3TextField(self.analysis_widgets['endomorph_card'], {
+            'name': 'triceps_text',
+            'position': (8, 48),
+            'width': self.analysis_widgets['endomorph_card'].width() - 16,
+            'labels': ('Tríceps (mm)', 'Triceps (mm)'),
+            'type': 'integer',
+            'size': 2,
             'theme': self.theme_value,
             'language': self.language_value,
             'text_edited': parent.on_textEdited } )
 
-        self.image_widgets['filename_button'] = MD3IconButton(self.image_widgets['image_form_card'], {
-            'name': 'filename_button',
-            'position': (width - 56, 82),
-            'type': 'tonal',
-            'icon': 'image', 
-            'theme': self.theme_value,
-            'clicked': parent.on_filename_button_clicked } )
-
-        self.image_widgets['line_1_divider'] = MD3Divider(self.image_widgets['image_form_card'], {
-            'name': 'line_1_divider',
-            'position': (8, 128),
-            'shape': 'horizontal',
-            'length': width - 32,
-            'theme': self.theme_value } )
-
-        self.image_widgets['left_minimum_XT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'left_minimum_XT_text',
-            'position': (8, 144),
-            'width': int((width - 80) / 2),
-            'labels': minimum_label,
-            'type': 'double',
-            'size': 7,
+        self.analysis_widgets['subescapular_text'] = MD3TextField(self.analysis_widgets['endomorph_card'], {
+            'name': 'subescapular_text',
+            'position': (8, 108),
+            'width': self.analysis_widgets['endomorph_card'].width() - 16,
+            'labels': ('Subescapular (mm)', 'Subescapular (mm)'),
+            'type': 'integer',
+            'size': 2,
             'theme': self.theme_value,
             'language': self.language_value,
             'text_edited': parent.on_textEdited } )
 
-        self.image_widgets['left_XT_label'] = MD3Label(self.image_widgets['image_form_card'], {
-            'name': 'left_XT_label',
-            'position': (16 + (width - 80) / 2, 158),
-            'type': 'icon',
-            'icon': 'left_foot',
-            'theme': self.theme_value } )
+        self.analysis_widgets['supraespinal_text'] = MD3TextField(self.analysis_widgets['endomorph_card'], {
+            'name': 'supraespinal_text',
+            'position': (8, 168),
+            'width': self.analysis_widgets['endomorph_card'].width() - 16,
+            'labels': ('Supraespinal (mm)', 'Supraespinal (mm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
 
-        self.image_widgets['left_maximum_XT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'left_maximum_XT_text',
-            'position': (56 + (width - 80) / 2, 144),
-            'width': int((width - 80) / 2),
-            'labels': maximum_label,
-            'type': 'double',
-            'size': 7,
+        self.analysis_widgets['pantorrilla_text'] = MD3TextField(self.analysis_widgets['endomorph_card'], {
+            'name': 'pantorrilla_text',
+            'position': (8, 228),
+            'width': self.analysis_widgets['endomorph_card'].width() - 16,
+            'labels': ('Pantorrilla (mm)', 'Calf (mm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+
+        # ---------------
+        # Card Mesomorphy
+        # ---------------
+        self.analysis_widgets['mesomorph_card'] = MD3Card(parent, { 
+            'name': 'mesomorph_card',
+            'position': ((width / 2) + 4, 8),
+            'size': ((width / 2) - 12, 468),
+            'type': 'filled',
+            'labels': ('Mesomorfismo', 'Mesomorphy'),
+            'theme': self.theme_value, 
+            'language': self.language_value } )
+        
+        self.analysis_widgets['altura_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'altura_text',
+            'position': (8, 48),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Altura (cm)', 'Height (cm)'),
+            'type': 'height_si',
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+
+        self.analysis_widgets['humero_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'humero_text',
+            'position': (8, 108),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Diámetro Húmero (cm)', 'Humerus Diameter (cm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+
+        self.analysis_widgets['femur_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'femur_text',
+            'position': (8, 168),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Diámetro Fémur (cm)', 'Femur Diameter (cm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+
+        self.analysis_widgets['biceps_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'biceps_text',
+            'position': (8, 228),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Perímetro Bíceps (cm)', 'Biceps Perimeter (cm)'),
+            'type': 'integer',
+            'size': 2,
             'theme': self.theme_value,
             'language': self.language_value,
             'text_edited': parent.on_textEdited } )
         
-        self.image_widgets['center_minimum_XT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'center_minimum_XT_text',
-            'position': (8, 204),
-            'width': int((width - 80) / 2),
-            'labels': minimum_label,
-            'type': 'double',
-            'size': 7,
+        self.analysis_widgets['tricipital_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'tricipital_text',
+            'position': (8, 288),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Pliegue Tricipital (cm)', 'Tricipital Fold (cm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+        
+        self.analysis_widgets['pantorrilla_perimetro_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'pantorrilla_perimetro_text',
+            'position': (8, 348),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Perímetro Pantorrilla (cm)', 'Calf Perimeter (cm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+        
+        self.analysis_widgets['pantorrilla_pliegue_text'] = MD3TextField(self.analysis_widgets['mesomorph_card'], {
+            'name': 'pantorrilla_pliegue_text',
+            'position': (8, 408),
+            'width': self.analysis_widgets['mesomorph_card'].width() - 16,
+            'labels': ('Pliegue Pantorrilla (cm)', 'Calf Fold (cm)'),
+            'type': 'integer',
+            'size': 2,
+            'theme': self.theme_value,
+            'language': self.language_value,
+            'text_edited': parent.on_textEdited } )
+        
+        # ---------------
+        # Card Ectomorphy
+        # ---------------
+        self.analysis_widgets['ectomorph_card'] = MD3Card(parent, { 
+            'name': 'ectomorph_card',
+            'position': (8, 304),
+            'size': ((width / 2) - 12, 108),
+            'type': 'filled',
+            'labels': ('Ectomorfismo', 'Ectomorphy'),
+            'theme': self.theme_value, 
+            'language': self.language_value } )
+        
+        self.analysis_widgets['weight_textfield'] = MD3TextField(self.analysis_widgets['ectomorph_card'], {
+            'name': 'weight_textfield',
+            'position': (8, 48),
+            'width': self.analysis_widgets['ectomorph_card'].width() - 16,
+            'labels': ('Peso (Kg)', 'Weight (Kg)'),
+            'type': 'weight',
             'theme': self.theme_value,
             'language': self.language_value,
             'text_edited': parent.on_textEdited } )
 
-        self.image_widgets['center_XT_label'] = MD3Label(self.image_widgets['image_form_card'], {
-            'name': 'center_XT_label',
-            'position': (16 + (width - 80) / 2, 218),
-            'type': 'icon',
-            'icon': 'feet',
-            'theme': self.theme_value } )
+        # --------------------------
+        # Card Buttons Ok and Cancel
+        # --------------------------
+        self.analysis_widgets['buttons_bar_card'] = MD3Card(parent, { 
+            'name': 'buttons_bar_card',
+            'position': (8, height - 56), 
+            'size': (width - 16, 48),
+            'type': 'filled',
+            'theme': self.theme_value, 
+            'language': self.language_value } )
 
-        self.image_widgets['center_maximum_XT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'center_maximum_XT_text',
-            'position': (56 + (width - 80) / 2, 204),
-            'width': int((width - 80) / 2),
-            'labels': maximum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['right_minimum_XT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'right_minimum_XT_text',
-            'position': (8, 264),
-            'width': int((width - 80) / 2),
-            'labels': minimum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['right_XT_label'] = MD3Label(self.image_widgets['image_form_card'], {
-            'name': 'right_XT_label',
-            'position': (16 + (width - 80) / 2, 278),
-            'type': 'icon',
-            'icon': 'right_foot',
-            'theme': self.theme_value } )
-
-        self.image_widgets['right_maximum_XT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'right_maximum_XT_text',
-            'position': (56 + (width - 80) / 2, 264),
-            'width': int((width - 80) / 2),
-            'labels': maximum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['line_2_divider'] = MD3Divider(self.image_widgets['image_form_card'], {
-            'name': 'line_2_divider',
-            'position': (8, 324),
-            'shape': 'horizontal',
-            'length': width - 32,
-            'theme': self.theme_value } )
-
-        self.image_widgets['left_minimum_YT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'left_minimum_YT_text',
-            'position': (8, 340),
-            'width': int((width - 80) / 2),
-            'labels': minimum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['left_YT_label'] = MD3Label(self.image_widgets['image_form_card'], {
-            'name': 'left_YT_label',
-            'position': (16 + (width - 80) / 2, 354),
-            'type': 'icon',
-            'icon': 'left_foot',
-            'theme': self.theme_value } )
-
-        self.image_widgets['left_maximum_YT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'left_maximum_YT_text',
-            'position': (56 + (width - 80) / 2, 340),
-            'width': int((width - 80) / 2),
-            'labels': maximum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['center_minimum_YT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'center_minimum_YT_text',
-            'position': (8, 400),
-            'width': int((width - 80) / 2),
-            'labels': minimum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['center_YT_label'] = MD3Label(self.image_widgets['image_form_card'], {
-            'name': 'center_YT_label',
-            'position': (16 + (width - 80) / 2, 414),
-            'type': 'icon',
-            'icon': 'feet',
-            'theme': self.theme_value } )
-
-        self.image_widgets['center_maximum_YT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'center_maximum_YT_text',
-            'position': (56 + (width - 80) / 2, 400),
-            'width': int((width - 80) / 2),
-            'labels': maximum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['right_minimum_YT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'right_minimum_YT_text',
-            'position': (8, 460),
-            'width': int((width - 80) / 2),
-            'labels': minimum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        self.image_widgets['right_YT_label'] = MD3Label(self.image_widgets['image_form_card'], {
-            'name': 'right_YT_label',
-            'position': (16 + (width - 80) / 2, 474),
-            'type': 'icon',
-            'icon': 'right_foot',
-            'theme': self.theme_value } )
-
-        self.image_widgets['right_maximum_YT_text'] = MD3TextField(self.image_widgets['image_form_card'], {
-            'name': 'right_maximum_YT_text',
-            'position': (56 + (width - 80) / 2, 460),
-            'width': int((width - 80) / 2),
-            'labels': maximum_label,
-            'type': 'double',
-            'size': 7,
-            'theme': self.theme_value,
-            'language': self.language_value,
-            'text_edited': parent.on_textEdited } )
-
-        # ---------------------
-        # Buttons Ok and Cancel
-        # ---------------------
-        self.image_widgets['cancel_button'] = MD3Button(self.image_widgets['image_form_card'], {
+        self.analysis_widgets['cancel_button'] = MD3Button(self.analysis_widgets['buttons_bar_card'], {
             'name': 'cancel_button',
-            'position': (width - 232, height - 56),
+            'position': (width - 232, 8),
             'width': 100,
             'type': 'text',
             'labels': ('Cancelar', 'Cancel'),
@@ -288,9 +245,9 @@ class AnalysisUI(QtWidgets.QDialog):
             'language': self.language_value,
             'clicked': parent.on_cancel_button_clicked } )
 
-        self.image_widgets['ok_button'] = MD3Button(self.image_widgets['image_form_card'], {
+        self.analysis_widgets['ok_button'] = MD3Button(self.analysis_widgets['buttons_bar_card'], {
             'name': 'ok_button',
-            'position': (width - 124, height - 56),
+            'position': (width - 124, 8),
             'width': 100,
             'type': 'text',
             'enabled': False,
