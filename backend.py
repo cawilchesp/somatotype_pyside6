a05d7d6dcac58e264675d021d847368d8f6aebde
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-08-25 20:12:38 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -22,14 +22,14 @@ def somatotype_calculation(measurements: dict) -> dict:
         anthropometric measurements:
         height (cm)
         weight (Kg)
-        triceps (mm)
-        subscapular (mm)
-        supraspinale (mm)
+        triceps skinfold (mm)
+        subscapular skinfold (mm)
+        supraspinale skinfold (mm)
         biceps (cm)
-        forearm (cm)
+        calf (cm)
         humerus (cm)
         femur (cm)
-        knee (cm)
+        calf skinfold (cm)
     
     Returns
     -------
@@ -38,7 +38,51 @@ def somatotype_calculation(measurements: dict) -> dict:
         endomorphy score
         mesomorphy score
         ectomorphy score
-        
+
+
+
+
+
+
+
+Determination of Somatotypes
+The somatotype values of the participants were determined using Heath Carter method of somatotyping. Based 
+on this method, body weight, height, biceps and calf circumference during flexion, humerus and femur breadth, 
+triceps, subscapular, suprailiac and calf skinfold as well as following formulas (Ross and Marfell-Jones, 1991; 
+Carter, 2002) are used to calculate somatotype values. In addition, “SOMATOTURK” program was used for 
+calculations (Marangoz and Özbalcı, 2017).
+
+2.1.1. Determination of Endomorph 
+A = triceps + subscapular + suprailiac
+B= (170.18 / height) (Adjustment coefficient for height)
+Adjusted sum X = A x B 
+Endomorph= - 0.7182 + 0.1451 (X) - 0.00068 (X^2) + 0.0000014 (X^3)
+
+2.1.2. Determination of Mesomorph
+Mesomorph = (0.858 HB + 0.601 FB +0.188 CAG + 0.161 CCG) - (0.131 H) + 4.5
+HB : Humerus breadth (cm) 
+FB : Femur breadth (cm) 
+CAG : Arm circumference during flexion – (Triceps skinfold /10 )
+CCG : Maximal calf circumference – (Calf skinfold /10)
+H : Height (cm)
+
+2.1.3. Determination of Ectomorph
+Height and weight are calculated in cm and kg, respectively. Height is divided by the cube root of weight to 
+calculate HWR (HWR=height/cube root of weight). Ectomorph is calculated based on HWR value using one of 
+the formulas below: 
+IF HWR ≥ 40.75, Ectomorph = 0.732× HWR– 28.58
+IF 38.25 < HWR < 40.75, Ectomorph = 0.463× HWR– 17.63
+IF HWR ≤ 38.25, Ectomorph = 0.1
+
+
+
+
+
+
+
+
+
+
     """
     height = int(measurements['height_value'])
     weight = int(measurements['weight_value'])
